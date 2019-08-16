247f09249f2d1177085ce0ced8b11a3291b7b63e
shu26 <skymid.26sk@gmail.com>
2019-08-16 13:08:28 +0900

    [Update] Update json2csv.py for vott2 format

--- json2csv.py
+++ json2csv.py
@@ -4,40 +4,42 @@ import json
 
 def main():
   # Where is the json file
-  f = open("anchi/images.json", 'r')
+  f = open("../../Desktop/Orthor_experiment/temp0730/tif_5m/others/vott-json-export/temp_ortho_5m-export.json", 'r')
   json_data = json.load(f)
-  image_list = json_data["visitedFrames"]
+  image_list = json_data["assets"]
   tag_list = []
 
   for image in image_list:
     # Where is the dataset directory
-    path = os.path.join("./anchi/images",image)
+    image_name = json_data["assets"][image]["asset"]["name"]
+    path = os.path.join("csv_data/temp0730/tif_5m/images/",image_name)
 
-    if not json_data["frames"][image]:
-      with open("./anchi/annotations/annotation.csv","a") as f:
+    if not json_data["assets"][image]["regions"]:
+      with open("../../Desktop/Orthor_experiment/temp0730/tif_5m/annotations/json_annotation.csv","a") as f:
         writer=csv.writer(f)
         writer.writerow([path,None,None,None,None,None])
 
-    for box in range(len(json_data["frames"][image])):
-      x1 = round(json_data["frames"][image][box]["box"]["x1"])
-      y1 = round(json_data["frames"][image][box]["box"]["y1"])
-      x2 = round(json_data["frames"][image][box]["box"]["x2"])
-      y2 = round(json_data["frames"][image][box]["box"]["y2"])
-      tag = json_data["frames"][image][box]["tags"][0]
+    for box in range(len(json_data["assets"][image]["regions"])):
+      bboxPath = json_data["assets"][image]["regions"][box]["boundingBox"]
+      x1 = round(bboxPath["left"])
+      y1 = round(bboxPath["top"])
+      x2 = round(bboxPath["left"]+bboxPath["width"])
+      y2 = round(bboxPath["top"]+bboxPath["height"])
+      tag = json_data["assets"][image]["regions"][box]["tags"][0]
 
       if not tag_list:
         tag_list.append(tag)
       if my_index_multi(tag_list, tag) == []:
         tag_list.append(tag)
       # Where is the annotation.csv (if you have not made it, it makes it)
-      with open("./anchi/annotations/annotation.csv","a") as f:
+      with open("../../Desktop/Orthor_experiment/temp0730/tif_5m/annotations/json_annotation.csv","a") as f:
         writer=csv.writer(f)
         writer.writerow([path,x1,y1,x2,y2,tag])
 
   for i in range(len(tag_list)):
     tag = tag_list[i]
     # Where is the test_class_id.csv (if you have not made it, it make it)
-    with open("./anchi/annotations/class_id.csv","a") as f:
+    with open("../../Desktop/Orthor_experiment/temp0730/tif_5m/annotations/json_class_id.csv","a") as f:
       writer=csv.writer(f)
       writer.writerow([tag,i])
 
